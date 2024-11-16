6543edf6a6328f05e0120e676ae511144410afb9
Mimooun <zouhrimimoun21@gmail.com>
2024-11-16 22:43:18 +0100

    first

---
+++ assets/js/login.js
@@ -0,0 +1,25 @@
+function login() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const message = document.getElementById("message");
+  
+    // Hardcoded credentials for example purposes
+    const correctUsername = "Sanaa";
+    const correctPassword = "fan2024";
+  
+    if (username === correctUsername && password === correctPassword) {
+      message.textContent = "Connexion réussie!";
+      message.style.color = "green";
+
+      // Set a login flag in localStorage
+      localStorage.setItem("isLoggedIn", "true");
+
+      // Redirect to index.html after successful login
+      setTimeout(() => {
+        window.location.href = "index.html";
+      }, 1000);
+    } else {
+      message.textContent = "Invalid username or password";
+      message.style.color = "red";
+    }
+}
