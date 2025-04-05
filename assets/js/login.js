5d26dc83c0a1d6989752ee9130dc9887af107658
mimounzouhri <zomimoun@etudiant-esic.fr>
2025-04-05 16:18:54 +0200

    remplacer le fichier json par js

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,32 +3,15 @@ function login() {
   const password = document.getElementById("password").value.trim();
   const message = document.getElementById("message");
 
-  fetch("/users.json")
-    .then(response => {
-      if (!response.ok) {
-        throw new Error("Erreur lors du chargement du fichier JSON");
-      }
-      return response.json();
-    })
-    .then(users => {
-      const user = users.find(u => u.username === username && u.password === password);
-      if (user) {
-        message.textContent = "Connexion réussie ✅";
-        message.style.color = "green";
-
-        // ⏳ Redirection après 1 seconde
-        setTimeout(() => {
-          window.location.href = "index.html"; // Chemin vers la page d’accueil
-        }, 1000);
-
-      } else {
-        message.textContent = "Identifiant ou mot de passe incorrect ❌";
-        message.style.color = "red";
-      }
-    })
-    .catch(error => {
-      console.error(error);
-      message.textContent = "Erreur serveur ou fichier introuvable.";
-      message.style.color = "red";
-    });
+  const user = users.find(u => u.username === username && u.password === password);
+  if (user) {
+    message.textContent = "Connexion réussie ✅";
+    message.style.color = "green";
+    setTimeout(() => {
+      window.location.href = "index.html";
+    }, 1000);
+  } else {
+    message.textContent = "Identifiants incorrects ❌";
+    message.style.color = "red";
+  }
 }
