95d3ba9da95810fde23207fb3899482da6247686
mimounzouhri <zomimoun@etudiant-esic.fr>
2025-04-05 16:31:08 +0200

    remplacer le fichier json par js2

--- assets/js/login.js
+++ assets/js/login.js
@@ -4,9 +4,15 @@ function login() {
   const message = document.getElementById("message");
 
   const user = users.find(u => u.username === username && u.password === password);
+
   if (user) {
+    // ✅ Enregistre que l'utilisateur est connecté
+    localStorage.setItem("isLoggedIn", "true");
+
     message.textContent = "Connexion réussie ✅";
     message.style.color = "green";
+
+    // Redirection vers la page d'accueil
     setTimeout(() => {
       window.location.href = "index.html";
     }, 1000);
